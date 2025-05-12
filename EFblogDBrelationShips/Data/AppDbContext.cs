a5d03fafc0d27e85c140700c1a613da79b7b0406
carlospenaforte <carlosedu_1132@hotmail.com>
2025-05-12 14:36:00 -0300

    ADD PACKAGES: ENTITY FRAMEWORK SQLSERVER, TOOLS AND DESIGN ;
    CREATED USER MODEL;
    
    CALLING THE DBCONTEXT IN DATA FOLDER.

---
+++ EFblogDBrelationShips/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFblogDBrelationShips.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFblogDBrelationShips.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
